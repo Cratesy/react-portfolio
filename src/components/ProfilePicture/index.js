bc40710a7d244db7ee7211defbdb9b70403940ae
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-07-28 16:57:29 +0100

    added profile picture component and removed unwanted react files

---
+++ src/components/ProfilePicture/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./index.css";
+
+function profilePicture() {
+  return (
+    <div>
+      <img
+        className="my-image"
+        src="/assets/img/My image for portfolio.jpg"
+        alt="Michael Crates"
+      />
+    </div>
+  );
+}
+
+export default profilePicture;
