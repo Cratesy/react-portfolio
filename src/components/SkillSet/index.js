25aa476998c6a269c5062bc47089450ff6e2487b
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-07-29 21:30:31 +0100

    added skill set icon bar with style component

---
+++ src/components/SkillSet/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { FaNodeJs, FaHtml5, FaReact, FaCss3Alt } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
+import { SiMysql, SiMongodb, SiGraphql } from "react-icons/si";
+
+import "./index.css";
+
+const skillSet = () => {
+  return (
+    <div className="skill-set">
+      <i className="logo" title="Node.js">
+        <FaNodeJs />
+      </i>
+      <i className="logo" title="HTML">
+        <FaHtml5 />
+      </i>
+      <i className="logo" title="React">
+        <FaReact />
+      </i>
+      <i className="logo" title="Css">
+        <FaCss3Alt />
+      </i>
+      <i className="logo" title="Javascript">
+        <IoLogoJavascript />
+      </i>
+      <i className="logo" title="MySQL">
+        <SiMysql title="MySQL" />
+      </i>
+      <i className="logo" title="MongoDB">
+        <SiMongodb title="MongoDB" />
+      </i>
+      <i className="logo" title="GraphQL">
+        <SiGraphql title="GraphQL" />
+      </i>
+    </div>
+  );
+};
+
+export default skillSet;
