c48ac6354d028f61765276ae907a967178213693
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-07-29 21:29:56 +0100

    added footer and style component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+import "./index.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <footer>
+        Built by Michael Crates&copy;
+        <a href="https://github.com/Cratesy">
+          <i className="logo">
+            <FaGithub title="Github" />
+          </i>
+        </a>
+        <a href="https://www.linkedin.com/in/michael-crates-538752200/">
+          <i className="logo">
+            <FaLinkedin title="LinkedIn" />
+          </i>
+        </a>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
