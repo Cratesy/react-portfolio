a5ccb732ad9cffd4f7d7497bc0c452ebd1f45c4b
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-07-29 21:31:26 +0100

    added profile picture componant with style

--- src/components/ProfilePicture/index.js
+++ src/components/ProfilePicture/index.js
@@ -2,16 +2,16 @@ import React from "react";
 
 import "./index.css";
 
-function profilePicture() {
+const profilePicture = () => {
   return (
     <div>
       <img
-        className="my-image"
+        className="my-image center"
         src="/assets/img/My image for portfolio.jpg"
         alt="Michael Crates"
       />
     </div>
   );
-}
+};
 
 export default profilePicture;
