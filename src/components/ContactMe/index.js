d781235b5cee7f9ed2cbef2aa6d99acaed698757
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-07-30 22:32:29 +0100

    added formspree and contact me form

---
+++ src/components/ContactMe/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
+
+import Button from "../Button";
+
+import "./index.css";
+
+const ContactMe = () => {
+  const [state, handleSubmit] = useForm("xwkarole");
+  if (state.succeeded) {
+    return <p>Thanks for your message, will respond ASAP!</p>;
+  }
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <div>
+        <input
+          className="input-box"
+          id="email"
+          type="email"
+          name="email"
+          placeholder="Email here!"
+        />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+      </div>
+      <div>
+        <textarea
+          className="input-box"
+          id="message"
+          name="message"
+          placeholder="Message here!"
+        />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+      </div>
+      <Button type="submit" disabled={state.submitting} name="Submit" />
+    </form>
+  );
+};
+
+export default ContactMe;
